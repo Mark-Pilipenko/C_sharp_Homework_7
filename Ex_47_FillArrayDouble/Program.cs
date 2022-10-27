7fd4d065cbdaee8fe3461c2d0e753b5bd30dd1c5
Mark <kalmark@mail.ru>
2022-10-27 22:49:09 +0300

    Add Ex_47_FillArrayDouble

---
+++ Ex_47_FillArrayDouble/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 47. 
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array= new double[m,n];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double fract = new Random().Next(0,10);
+            matrix[i,j] = new Random().Next(0,10) + fract/10;
+        }
+    }
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
